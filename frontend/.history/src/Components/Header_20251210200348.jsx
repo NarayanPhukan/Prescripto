623b0755182d46822c2ed35c7c73c3ee969c1f7a
NarayanPhukan <narayanphukan30@gmail.com>
2025-12-10 20:06:05 +0530

    Book button updated

--- frontend/.history/src/Components/Header_20251210200348.jsx
+++ frontend/.history/src/Components/Header_20251210200348.jsx
@@ -17,7 +17,7 @@ const Header = () => {
             schedule your appointment hassle-free.
           </p>
         </div>
-        <a href="#speciality" className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300 hover:bg-[#080e87] hover:text-white">
+        <a href="#speciality" className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300 hover:bg-[#084887] hover:text-white">
             Book appointment <img className="w-3" src={assets.arrow_icon} alt="" />
         </a>
       </div>
